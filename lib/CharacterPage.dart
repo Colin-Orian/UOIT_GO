fb0a6f625f595bd1831fb3e2b9cbd225935859a3
Dooomino <yudi.tao@ontariotechu.net>
2019-11-05 21:44:08 -0500

    Add Character class, remove the debug banner.

--- lib/CharacterPage.dart
+++ lib/CharacterPage.dart
@@ -8,3 +8,26 @@ class CharacterPage extends StatelessWidget{
     );
   }
 }
+
+class Character{
+  double health;
+  double motivation;
+
+  List _inventory =[];
+  List _loadout = [];
+  List _activities = [];
+
+  Character({health,motivation});
+
+  List getInv(){
+    return _inventory;
+  }
+
+  List getLoad(){
+    return _loadout;
+  }
+  
+  List getAct(){
+    return _activities;
+  }
+}
