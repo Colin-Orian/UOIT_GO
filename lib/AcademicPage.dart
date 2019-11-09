0190598bfe41089e3c88b066dded2aca702fc271
Dooomino <yudi.tao@ontariotechu.net>
2019-11-09 01:46:41 -0500

    add score color and create a switch for overall process.

--- lib/AcademicPage.dart
+++ lib/AcademicPage.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'Course.dart';
+import 'package:flip_card/flip_card.dart';
 
 class AcademicPage extends StatefulWidget{
   final String title;
@@ -12,53 +13,174 @@ class AcademicPage extends StatefulWidget{
 }
 
 class AcademicPageState extends State<AcademicPage>{
-  int index=0;
+  //test
+  List courses = <Course>[
+    Course("CSCI 4100U","Mobile Development",95.0),
+    Course("CSCI 4100U","Mobile Development",63.0),
+    Course("CSCI 4100U","Mobile Development",50.0),
+    Course("CSCI 4100U","Mobile Development",75.0),
+    Course("CSCI 4100U","Mobile Development",45.0),
+    Course("CSCI 4100U","Mobile Development",72.0),
+    Course("CSCI 4100U","Mobile Development",38.0),
+    Course("CSCI 4100U","Mobile Development",72.0),
+    Course("CSCI 4100U","Mobile Development",89.0),
+  ];
+
+  bool showOverall = false;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListView(
-        children: <Widget>[
-          _buildCoures(Course("CSCI 4100U","Mobile Development"),index++),
-          _buildCoures(Course("CSCI 4100U","Mobile Development"),index++),
-          _buildCoures(Course("CSCI 4100U","Mobile Development"),index++),
-          _buildCoures(Course("CSCI 4100U","Mobile Development"),index++),
-          _buildCoures(Course("CSCI 4100U","Mobile Development"),index++),
-        ],
-      ),
-    );
-  }
-  Card _buildCoures(Course course,int index){
-    return Card(
-      color: Colors.blue[100*index],
-      margin: EdgeInsets.symmetric(vertical: 5.0),
-      clipBehavior: Clip.antiAlias,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children:<Widget>[
-          Container(
-            padding: EdgeInsets.symmetric(horizontal:10.0,vertical:20.0),
-            child: Text(course.courseName,textAlign: TextAlign.center,style: TextStyle(
-              fontSize: 18.0
-            ),)
-          ),
-          Padding(
-            //color: Colors.amber,
-            padding: EdgeInsets.all(5.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                    child: Text(course.courseId,textAlign: TextAlign.center,)
-                ),
-                Container(
-                  child:  Text(course.time,textAlign: TextAlign.center,)
-                ),
+    List builds = <Widget>[
+            _buildCoures(courses[0]),
+            _buildCoures(courses[1]),
+            _buildCoures(courses[2]),
+            _buildCoures(courses[3]),
+            _buildCoures(courses[4]),
+            _buildCoures(courses[5]),
+            _buildCoures(courses[6]),
+            _buildCoures(courses[7]),
+            _buildCoures(courses[8]),
+          ];
+    List widgets = <Widget>[
+      Container(
+        child: Card(
+          child: Row(
+            children: <Widget>[
+              Text("Show total grades"),
+              Switch(
+                value: showOverall,
+                onChanged: (value){},
+              ),
               ],
             ),
           ),
-        ],
+      )
+    ];
+    widgets.addAll(builds);
+    return  ListView(
+        children: widgets
+      );
+  }
+
+  Container _buildCoures(Course course){
+    Color backgroundColor = course.getColor();
+    Color textColorFront = getTextColor(Colors.white);
+    Color textColorBack = getTextColor(backgroundColor);
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: <BoxShadow>[
+            BoxShadow(color: Colors.black38,offset: Offset(0, 2.0),blurRadius: 2.0)
+          ],
+      ),
+      margin: EdgeInsets.only(bottom: 5.0),
+      child: FlipCard(
+        direction: FlipDirection.VERTICAL,
+        front: Container(
+          color: Colors.white,
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children:<Widget>[
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal:12.0),
+                    child: CircleAvatar(
+                      child: Icon(
+                          Icons.edit,
+                          size: 24.0,
+                        ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal:10.0,vertical:20.0),
+                    child: Text(course.courseName,textAlign: TextAlign.center,style: TextStyle(
+                      fontSize: 18.0,
+                      color: textColorFront,
+                      ),
+                    )
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(5.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                            child: Text(course.courseId,textAlign: TextAlign.center,style: TextStyle(
+                              color: textColorFront,
+                              ),
+                            )
+                        ),
+                        Container(
+                          child:  Text(course.time,textAlign: TextAlign.center,style: TextStyle(
+                              color: textColorFront,
+                            ),)
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                child:Text("Tap to see grade.",textAlign: TextAlign.center,style: TextStyle(
+                    color: textColorFront,
+                    fontSize: 12.0,
+                ),)
+              )
+            ],
+          ),
+        ),
+        back: Container(
+          color: backgroundColor,
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children:<Widget>[
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal:10.0,vertical:20.0),
+                    child: Text(course.courseName,textAlign: TextAlign.center,style: TextStyle(
+                      fontSize: 18.0,
+                      color: textColorBack,
+                      ),
+                    )
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal:30.0,vertical:20.0),
+                    child: Text("${course.grade.toString()}%",textAlign: TextAlign.center,style: TextStyle(
+                        fontSize: 18.0,
+                        color: textColorBack, 
+                        ),
+                      ),
+                  )
+                ]
+              ),
+              Container(
+                child:Text("",textAlign: TextAlign.center,style: TextStyle(
+                    color: textColorBack,
+                    fontSize: 12.0,
+                ),)
+              )
+            ],
+          ),
+        ),
       ),
     );
   } 
+
+  Color getTextColor(Color background){
+    int white = 0;
+    double a1 = 0.299;
+    double a2 = 0.587;
+    double a3 = 0.114;
+
+    double l = (a1*background.red + a2*background.green + a3*background.blue)/255.0;
+
+    if(l>0.5)
+      white=0;
+    else
+      white=255;
+
+    return Color.fromARGB(255, white, white, white);
+  }
+
 }
--- lib/Course.dart
+++ lib/Course.dart
@@ -1,17 +1,36 @@
+
+import 'package:flutter/material.dart';
+
 class Course{
   String courseId;
   String courseName;
-
+  double grade;
   String time;
 
-  Course(this.courseId,this.courseName){
+  Course(this.courseId,this.courseName,this.grade){
     //TODO pull time from database
     //  Suggest To put weekdays
-    this.time = DateTime.now().toLocal().toString().split(' ')[0];    
+    this.time = "Wed";    
   }
+  
+  int getColorValue(){
+    int color = (9*this.grade/100).floor()*100;
+    return 900-color;
+  }
+
+  Color getColor(){
+    if(this.grade < 50){
+      return Colors.red[this.getColorValue()];
+    }else if(this.grade>=50 && this.grade < 75){
+      return Colors.amber[this.getColorValue()];
+    }else if(this.grade>=75 && this.grade < 90){
+      return Colors.blue[this.getColorValue()];
+    }
+    return Colors.green;
+  } 
   
   @override
   String toString(){
-    return "${this.courseId}:${this.courseName}@${this.time}";
+    return "${this.courseId}:${this.courseName}@${this.time} - ${this.grade}%";
   }
 }
