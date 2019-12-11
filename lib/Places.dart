ea2c513f3824dc6587e2671cc16f4823eb6bfbfe
Colin Orian <corian@rogers.com>
2019-12-11 15:32:00 -0500

    Detects when you're in UA, ERC, Library. Doesn't work for UB yet.

--- lib/Places.dart
+++ lib/Places.dart
@@ -4,29 +4,32 @@ class _Point{
   _Point({this.x, this.y});
 }
 
-
 class Places{
   static List<_Point> _botLeft = [
-    _Point(x: 43.9443203, y: -78.8958327),
-    _Point(x: 43.945397, y: -78.8958327),
-    _Point(x: 43.945849, y: -78.89642),
+    _Point(x: 43.944156, y: -78.897096), //UA
+    _Point(x: 43.944832, y: -78.895803), //UB
+    _Point(x: 43.945420, y: -78.896484), //ERC
+    _Point(x: 43.945540, y: -78.897450), //Library
   ];
 
   static List<_Point> _topRight = [
-    _Point(x: 43.9447362, y: -78.8971068),
-    _Point(x: 43.954133, y: -78.8959179),
-    _Point(x: 43.9459111, y: -78.8960908),
+    _Point(x: 43.944832, y: -78.895803), //UA
+    _Point(x: 43.945377, y: -78.895921), //UB
+    _Point(x: 43.945907, y: -78.896066), //ERC
+    _Point(x: 43.946057, y: -78.897037), //Library
   ];
   static List<String> _places = [
     'UA',
-    'UL',
+    'UB',
     'ERC',
+    'Library'
   ];
 
   static String currentPlace(double lat, double long){
     for(int i = 0; i < _places.length; i ++){
-      if(lat >=_botLeft[i].x && lat <= _topRight[i].x){
-        return _places[i];
+      if(lat >=_botLeft[i].x && lat <= _topRight[i].x
+        && long >=_botLeft[i].y && long <=_topRight[i].y){
+          return _places[i];
       }
     }
     return 'No Location';
